d285b4c8c51e7510288bc400e94f0ca80b194852
HamzaMurghay <hamzamurghay@gmail.com>
2025-05-18 05:20:28 +0400

    x

--- web scrape games/scrape_pc_playable_games.py
+++ web scrape games/scrape_pc_playable_games.py
@@ -2,10 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+
+with open("cookies.txt") as f:
+    x = f.read()
+
+x = x.splitlines()
+
+cookie_val = {line.split("\t")[0]:line.split("\t")[1] for line in x}
+
+print(cookie_val)
+
 url = "https://www.pcgamebenchmark.com/compatible-games"
 
 page = requests.get(url)
 
 chai = BeautifulSoup(page.text, 'lxml')
 
-print(chai)
+# print(chai)
